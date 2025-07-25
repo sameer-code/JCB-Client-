c382c8cc0b8bed0d71b47feef66b4817fff9c1e4
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-26 02:05:52 +0530

    Update and rename Stores.js to StoresList.js

---
+++ src/pages/StoresList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoreList = () => {
+  return (
+    <div className="p-6 text-center text-lg text-gray-700">
+      This is the Store List Page
+    </div>
+  );
+};
+
+export default StoreList;
