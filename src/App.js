097cf734f468b85c22bcb42063bab9f7ebdba058
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-25 12:08:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Wallet from './pages/Wallet';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Wallet from "./pages/Wallet";
+import StoreList from "./pages/StoreList";
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav style={{ padding: '10px', background: '#f0f0f0' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-        <Link to="/wallet" style={{ marginRight: '10px' }}>Wallet</Link>
-        <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-        <Link to="/register">Register</Link>
-      </nav>
-
+    <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/wallet" element={<Wallet />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/stores" element={<StoreList />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
