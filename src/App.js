fee9ed64c11cc882196aa3665befc89818f65ef2
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-26 00:38:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 // src/App.js
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import your pages
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Wallet from './pages/Wallet';
-import StoreList from './pages/StoreList';
-
-// Import Navbar
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Stores from "./pages/Stores";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Wallet from "./pages/Wallet";
 
 function App() {
   return (
@@ -19,9 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/stores" element={<StoreList />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/stores" element={<Stores />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/wallet" element={<Wallet />} />
       </Routes>
     </Router>
