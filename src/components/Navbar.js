a78f906f3559a85a6839cc161c70141bc35c835a
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-25 15:02:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className="bg-gray-900 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-bold">
-          <Link to="/">Just Cash Back</Link>
+          <Link to="/" className="text-white hover:text-yellow-400">Just Cash Back</Link>
         </div>
 
         <div className="md:hidden">
@@ -20,21 +20,21 @@ function Navbar() {
           </button>
         </div>
 
-        <ul className={`md:flex space-x-6 ${isOpen ? "block" : "hidden"} md:block`}>
+        <ul className={`md:flex flex-col md:flex-row md:items-center gap-4 ${isOpen ? "block" : "hidden"} md:block`}>
           <li>
-            <Link to="/" className="hover:text-yellow-400">Home</Link>
+            <Link to="/" className="text-white hover:text-yellow-400">Home</Link>
           </li>
           <li>
-            <Link to="/stores" className="hover:text-yellow-400">Stores</Link>
+            <Link to="/stores" className="text-white hover:text-yellow-400">Stores</Link>
           </li>
           <li>
-            <Link to="/register" className="hover:text-yellow-400">Register</Link>
+            <Link to="/register" className="text-white hover:text-yellow-400">Register</Link>
           </li>
           <li>
-            <Link to="/login" className="hover:text-yellow-400">Login</Link>
+            <Link to="/login" className="text-white hover:text-yellow-400">Login</Link>
           </li>
           <li>
-            <Link to="/wallet" className="hover:text-yellow-400">Wallet</Link>
+            <Link to="/wallet" className="text-white hover:text-yellow-400">Wallet</Link>
           </li>
         </ul>
       </div>
