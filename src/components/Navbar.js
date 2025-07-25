a5f4693069de9577509ab92f897bb57787996dde
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-25 14:29:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // optional for styling
 
 function Navbar() {
   return (
     <nav style={styles.nav}>
       <div style={styles.logo}>Just Cash Back</div>
       <ul style={styles.menu}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/stores">Stores</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/wallet">Wallet</Link></li>
+        <li><Link to="/" style={styles.link}>Home</Link></li>
+        <li><Link to="/stores" style={styles.link}>Stores</Link></li>
+        <li><Link to="/register" style={styles.link}>Register</Link></li>
+        <li><Link to="/login" style={styles.link}>Login</Link></li>
+        <li><Link to="/wallet" style={styles.link}>Wallet</Link></li>
       </ul>
     </nav>
   );
@@ -34,6 +33,12 @@ const styles = {
     listStyle: "none",
     display: "flex",
     gap: "15px",
+    margin: 0,
+    padding: 0,
+  },
+  link: {
+    color: "#fff",
+    textDecoration: "none",
   },
 };
 
