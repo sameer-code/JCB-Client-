fab69c0792e987a6a02d49f663d3401750c82836
sameer-code <60299015+sameer-code@users.noreply.github.com>
2025-07-26 13:13:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Pages
 import Home from "./pages/Home";
-import StoreList from "./pages/StoreList";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Wallet from "./pages/Wallet";
+import Stores from "./pages/Stores";
 
 function App() {
   return (
     <Router>
-      <div className="bg-white min-h-screen">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/stores" element={<StoreList />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/wallet" element={<Wallet />} />
-        </Routes>
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/stores" element={<Stores />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/wallet" element={<Wallet />} />
+          </Routes>
+        </main>
+
+        <Footer />
       </div>
     </Router>
   );
